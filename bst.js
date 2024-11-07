ec60930ea6045e3a1fba2945ce04a669923a3088
Lydos <lydossky@gmail.com>
2024-11-07 18:01:05 +0300

    inorder walk and finding value node

--- bst.js
+++ bst.js
@@ -6,9 +6,9 @@ function Tree(array) {
   const root = buildTree(array);
 
   function buildTree(array) {
-    // First sort then remove duplicates
     if (array.length === 1) return new Node(array[0], null, null);
     if (array.length === 0) return null;
+    // First sort then remove duplicates
     const sortedArray = Array.from(new Set(array.sort((a, b) => a - b)));
     const mid = Math.floor(sortedArray.length / 2);
     const leftTree = sortedArray.slice(0, mid);
@@ -21,23 +21,60 @@ function Tree(array) {
     );
   }
 
-  function prettyPrint(node, prefix = "", isLeft = true) {
-    if (node === null) {
-      return;
+  function insert(value) {}
+  function deleteItem(value) {}
+
+  // Searches for value inside the tree
+  // if it not exist returns null
+  function find(value) {
+    function helper(node) {
+      if (node === null || node.data === value) {
+        return node;
+      }
+      return value < node.data ? helper(node.left) : helper(node.right);
     }
 
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-    }
-
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-
-    if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-    }
+    return helper(root);
   }
 
-  return { root, prettyPrint };
+  function levelOrder(callback) {}
+  // Traverses the tree and call callback
+  // on each node
+  function inOrder(callback) {
+    if (callback === undefined) {
+      throw new Error("Callback is required");
+    }
+
+    function helper(node) {
+      if (node === null) return;
+      helper(node.left);
+      callback(node);
+      helper(node.right);
+    }
+
+    helper(root);
+  }
+  function preOrder(callback) {}
+  function postOrder(callback) {}
+  function height(node) {}
+  function depth(node) {}
+  function isBalanced() {}
+  function rebalance() {}
+
+  return {
+    root,
+    insert,
+    deleteItem,
+    find,
+    levelOrder,
+    inOrder,
+    preOrder,
+    postOrder,
+    height,
+    depth,
+    isBalanced,
+    rebalance,
+  };
 }
 
 export default Tree;
